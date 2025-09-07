9f0d9a388f348b1a266e055c8f04146e0f24a585
Abderrahman-byte <el.abderrahman00@gmail.com>
2025-09-07 19:41:09 +0100

    fxmt

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -1,9 +1,19 @@
 const siteConfig = {
-    name: 'PlombierPro',
+    name: 'HydroPlombier',
     telephone: '+32 467 60 12 47',
-    url: 'https://plombierpro.be',
-    image: 'https://plombierpro.be/hero-plumber.jpg',
-    openingHours: ['Mo-Su 00:00-23:59'],
+    url: 'https://www.hydroplombier.com',
+    image: 'https://www.hydroplombier.com/hero-plumber.jpg',
+    openingDayOfWeek: [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday',
+    ],
+    openingOpens: '00:00',
+    openingCloses: '23:59',
 }
 
 export default siteConfig
